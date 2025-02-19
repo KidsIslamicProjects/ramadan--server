7825987972fcd7487d4778c09bd243551b9b5653
sscout-challenges <hkhodor29@gmail.com>
2025-02-19 16:38:18 +0200

    Update User Model

--- models/Daily.js
+++ models/Daily.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const dayDataSchema = new mongoose.Schema({
-  hijriDate: { type: String, required: true }, // Hijri date (e.g., "الأوّل من رمضان")
-  hadith: { type: String, required: true }, // Hadith text
-  tafseerQuestion: { type: String, required: true }, // Tafseer question
-  tafseerAnswer: { type: String, required: true }, // Tafseer answer
+  hijriDate: { type: String, required: true },
+  hadith: { type: String, required: true },
+  tafseerQuestion: { type: String, required: true },
+  tafseerAnswer: { type: String, required: true },
 });
 
 const DayData = mongoose.model("DayData", dayDataSchema);
--- models/User.js
+++ models/User.js
@@ -4,6 +4,13 @@ const userSchema = new mongoose.Schema({
   name: { type: String, unique: true },
   password: { type: String },
   age: { type: Number },
+  dailyProgress: [
+    {
+      hijriDate: { type: String },
+      score: { type: Number, default: 0 },
+      tafseerAnswer: { type: String },
+    },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
