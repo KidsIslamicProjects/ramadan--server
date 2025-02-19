ba4d218c976cb3b84a60b3f7468b58a6f9bc60c7
sscout-challenges <hkhodor29@gmail.com>
2025-02-19 14:18:17 +0200

    Add urls to cors

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const tasksRoutes = require("./routes/dailyRoutes.js");
 const app = express();
 const PORT = 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://ramadan-challenge.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 mongoose
